712f57a9c305eada3edf6a6ed85a3e7c79c09ff0
Hugo Iboa <iboazain@gmail.com>
2025-11-23 14:18:07 -0800

    Added PasswordStrengthResult.java model class

--- src/passwordanalyzer/PasswordStrengthResult.java
+++ src/passwordanalyzer/PasswordStrengthResult.java
@@ -1,4 +1,28 @@
 package passwordanalyzer;
 
+import java.util.List;
+
 public class PasswordStrengthResult {
+
+    private final int score;
+    private final String label;
+    private final List<String> feedback;
+
+    public PasswordStrengthResult(int score, String label, List<String> feedback){
+        this.score = score;
+        this.label = label;
+        this.feedback = feedback;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<String> getFeedback() {
+        return feedback;
+    }
 }
